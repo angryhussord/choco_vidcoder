f5ee73420c270e4b2ee87227bd020b58ace55c28
angryhussord <angryhussord@outlook.com>
2014-11-14 10:14:10 -0800

    Adding initial files.

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'vidcoder' 
+$installerType = 'exe' 
+$url = 'https://vidcoder.codeplex.com/downloads/get/926918/#' 
+$url64 = 'https://vidcoder.codeplex.com/downloads/get/926919/#' 
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+$validExitCodes = @(0) 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes

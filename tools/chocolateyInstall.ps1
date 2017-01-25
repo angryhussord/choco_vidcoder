64ea18f6a8247f185054571842db7ee48d197322
Patrick Hufford <pahuff@microsoft.com>
2017-01-25 13:37:58 -0800

    2.47 stable update
    huge jump in numbering, but otherwise this is just the latest version

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,24 @@
-﻿$packageName = 'vidcoder' 
-$installerType = 'exe' 
-$url = 'https://github.com/RandomEngy/VidCoder/releases/download/v1.5.34/VidCoder-1.5.34-x86.exe' 
-$url64 = 'https://github.com/RandomEngy/VidCoder/releases/download/v1.5.34/VidCoder-1.5.34-x64.exe' 
-$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
-$validExitCodes = @(0) 
+﻿$ErrorActionPreference = 'Stop';
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
+$packageName = 'vidcoder'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
+$installerType = 'EXE'
+$url = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.47/VidCoder-2.47-x86.exe' 
+$url64 = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.47/VidCoder-2.47-x64.exe' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = $installerType
+  url           = $url
+  url64bit      = $url64
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+  softwareName  = 'vidcoder*'
+  checksum      = '9B725026697A8C3604F7A597DDF4CF38'
+  checksumType  = 'sha256'
+  checksum64      = 'BB9034B6C616D717507F9BAD5173C784'
+  checksumType64  = 'sha256'
+};
+
+Install-ChocolateyPackage @packageArgs;

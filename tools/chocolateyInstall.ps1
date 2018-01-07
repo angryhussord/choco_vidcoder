6f22d51228a9814af152847777b5934b5a59f947
Patrick Hufford <pahuff@microsoft.com>
2018-01-07 06:13:30 -0800

    2.62 update

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageName = 'vidcoder'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE'
-$url = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.58/VidCoder-2.58-x86.exe' 
-$url64 = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.58/VidCoder-2.58-x64.exe' 
+$url = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.62/VidCoder-2.62-x86.exe' 
+$url64 = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.62/VidCoder-2.62-x64.exe' 
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'vidcoder*'
-  checksum      = '653056928A5231C488A5F9F07D4BACC331B8B389731090F1CF3664145F3DAE56'
+  checksum      = '49A0E7F624241F81CFD3C85DA9150FB19B25A09A8D52BB3D3170B504BB4FC185'
   checksumType  = 'sha256'
-  checksum64      = '71CF321FFD4D8E610A9D9524143030E1ED36851FC361311F6B449F75EF306F52'
+  checksum64      = '85CFB20DFE47D4341CD29182BD60EE49277F7505B32AA0CD3721EFBF0D403238'
   checksumType64  = 'sha256'
 };
 

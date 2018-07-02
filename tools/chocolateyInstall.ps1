2643fe0d4fb7884537ca0a83ebf854376fd3c370
Patrick Hufford <pahuff@microsoft.com>
2018-07-02 08:09:44 -0700

    3.15 update
    With this update, the x86 version is no longer being distributed. Also,
    the author has launched a Windows Store version of the app for download
    from the Store as well.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,22 +3,18 @@
 $packageName = 'vidcoder'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE'
-$url = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.62/VidCoder-2.62-x86.exe' 
-$url64 = 'https://github.com/RandomEngy/VidCoder/releases/download/v2.62/VidCoder-2.62-x64.exe' 
+$url = 'https://github.com/RandomEngy/VidCoder/releases/download/v3.15/VidCoder-3.15.exe' 
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = $installerType
   url           = $url
-  url64bit      = $url64
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'vidcoder*'
-  checksum      = '49A0E7F624241F81CFD3C85DA9150FB19B25A09A8D52BB3D3170B504BB4FC185'
+  checksum      = '3E8FD9E98A36AFA9A0F51BA9D992F14D42B6762C94D13FBC1D1295C4686AD93A'
   checksumType  = 'sha256'
-  checksum64      = '85CFB20DFE47D4341CD29182BD60EE49277F7505B32AA0CD3721EFBF0D403238'
-  checksumType64  = 'sha256'
 };
 
 Install-ChocolateyPackage @packageArgs;
